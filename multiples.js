bfadb8d7f0ff7f686eef913a8e5e47bf7ab45350
Taylor Kennedy <taykenned@gmail.com>
2016-03-08 09:33:21 -1000

    Adds missing method and tests

--- multiples.js
+++ multiples.js
@@ -5,8 +5,12 @@
  */
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
-
-  // do your work here
-
+  for(var i = 0; i < n; i++)
+    if(isMultiple(i)) sum += i;
   return sum;
 };
+
+var isMultiple = function(n) {
+  if(n % 3 === 0 || n % 5 === 0) return true;
+  return false
+}
